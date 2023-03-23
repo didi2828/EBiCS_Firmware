7eaf012d29a82bc30d6470f80058490cdd1188aa
didi2828 <100091291+didi2828@users.noreply.github.com>
2023-03-23 19:17:21 +0100

    SPEEDSOURCE INTERNAL; EXTERNAL 1;  INTERNAL 0

--- Inc/config.h
+++ Inc/config.h
@@ -72,9 +72,9 @@
 #define AUTODETECT 0
 #define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
 
-#define EXTERNAL 0
-#define INTERNAL 1
-#define SPEEDSOURCE EXTERNAL
+#define EXTERNAL 1
+#define INTERNAL 0
+#define SPEEDSOURCE INTERNAL
 #define SPEEDFILTER 1
 #define SIXSTEPTHRESHOLD 20000
 
