17c5170c18d3ab280874c28f0cc4ca20932df1b1
didi2828 <100091291+didi2828@users.noreply.github.com>
2023-04-05 16:10:57 +0200

    I_FACTOR_SPEED 20 (10 is old value)

--- Inc/config.h
+++ Inc/config.h
@@ -36,7 +36,7 @@
 #define P_FACTOR_I_D 50
 #define I_FACTOR_I_D 2
 #define P_FACTOR_SPEED 1
-#define I_FACTOR_SPEED 10
+#define I_FACTOR_SPEED 20
 #define TS_COEF 16000000
 #define PAS_TIMEOUT 8000
 #define RAMP_END 1200
